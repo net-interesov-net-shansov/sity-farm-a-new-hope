cff84e6ce8c9165e10733bf797f2e7e31d64bfac
net-interesov-net-shansov <yaroslav.ishutin@gmail.com>
2024-01-17 16:08:20 +0300

    Откат, отказ от threads
    организовать вывод через JS

--- testDHT/DHT.py
+++ testDHT/DHT.py
@@ -3,47 +3,25 @@ import socket
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 import threading
+import time
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
 
 app = Flask(__name__)
 
-
-
-sensor = Adafruit_DHT.DHT22     # указываем тип датчика
-pin = 5     # указываем пин на реле, к которому подсоединён датчик
-relay = 29      # указываем пин на распберри, соответствующий пину на реле
-
-hum = None
-temp = None
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(relay, GPIO.OUT)
-GPIO.output(relay, GPIO.HIGH)
-
-
-def DHT22():
-	global hum,  temp
-	while True:
-		hum,  temp = Adafruit_DHT.read_retry(sensor, pin)
-		print("h = ", hum, "t = ", temp)
-		
-
-
-@app.route("/DHT")
-def get_DHT22():
-    if hum is not None and temp is not None:
-        hum_string, temp_string = (hum, temp)
-        return jsonify(humidity = hum_string, temperature = temp_string)
-    
-    GPIO.output(relay, GPIO.LOW)
+@app.route('/')
+def DHT():
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        return jsonify(humidity, temperature)
     GPIO.cleanup()
-    
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(('8.8.8.8', 80))
 
-	
-thread = threading.Thread(target=DHT22)
-thread.start()
-	
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+
 if __name__ == '__main__':
-	app.run(host=(s.getsockname()[0]), port=5000, debug=True)
+    app.run(host=(s.getsockname()[0]), port = 5000, debug=True)
