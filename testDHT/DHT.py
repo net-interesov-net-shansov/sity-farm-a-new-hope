0e35f2801944afac4b3206e2274bda9b901747c4
net-interesov-net-shansov <yaroslav.ishutin@gmail.com>
2024-01-25 16:09:33 +0300

    SocketIO - стадия осознания

--- testDHT/DHT.py
+++ testDHT/DHT.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template, redirect, url_for, session
+from flask_socketio import SocketIO
 import socket
 import Adafruit_DHT
 import RPi.GPIO as GPIO
@@ -8,23 +9,27 @@ import mh_z19
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-# ___--- Сменить режим нумерации на GPIO.BOARD, тк это режим не зависит от модели 
-# ___--- распберри и более универсален
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def SENSORS():
+    session.clear()
+    GPIO.cleanup()
+    
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
-        return humidity,temperature
-    GPIO.cleanup()
+        return render_template('DHT.html', humidity, temperature)
+    else:
+        return render_template('DHT.html', error="Sensor does not return valid response")
+    
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 
 if __name__ == '__main__':
-    app.run(host=(s.getsockname()[0]), port = 5000, debug=True)
+    socketio.run(app, host=(s.getsockname()[0]), port = 5000, debug=True)
